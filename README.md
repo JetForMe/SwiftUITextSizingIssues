14cf5446c9a447cd2f9b9bc49deada0d28208f03
Rick M <rmann@latencyzero.com>
2024-01-18 18:02:49 -0800

    First checkin.

---
+++ README.md
@@ -0,0 +1,18 @@
+This sample project shows various view sizing issues with a simple SwiftUI view.
+
+## Desired
+
+* The containing window should be fixed to encompass the contents as tightly
+	as possible.
+* The width should be the width of the image, always.
+* The height should grow as necessary (up to the available screen height)
+	to accommodate the contents.
+
+## Actual
+
+* The window can be resized horizontally because the text is wider than the
+	containing view.
+* It can be resized vertically because some of the vertical padding is not
+	respected by the `.windowResizability(.contentSize)` modifier.
+* The only way I can get the text to properly wrap is to use `.fixedSize(horizontal: false, vertical: true)`,
+	which causes it to blow up the vertical spacing.
--- SwiftUISizingIssues/SwiftUISizingIssuesApp.swift
+++ SwiftUISizingIssues/SwiftUISizingIssuesApp.swift
@@ -8,10 +8,81 @@
 import SwiftUI
 
 @main
-struct SwiftUISizingIssuesApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+struct SwiftUISizingIssuesApp: App
+{
+	var body: some Scene
+	{
+		Window("SwiftUI is !Fun", id: "window")
+		{
+			ContentView()
+		}
+		.windowResizability(.contentSize)
+	}
+}
+
+struct
+ContentView: View
+{
+	var
+	body: some View
+	{
+		VStack(spacing: 0.0)
+		{
+			VStack
+			{
+				Text(
+				"""
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+				""")
+					.lineLimit(nil)
+//					.fixedSize(horizontal: false, vertical: true)
+//					.frame(height: 100)
+					.font(.system(size: 24.0))
+					.padding(.top)
+				
+//				DebugView()					Ignore this for now.
+			}
+			.padding([.leading, .trailing])
+			
+			Image("keyboard")
+		}
+	}
+}
+
+#Preview
+{
+	ContentView()
+}
+
+/**
+	Ignore this for now. In my full app I have more issues with this
+	view that aren’t reproduced in this sample yet.
+*/
+
+struct
+DebugView : View
+{
+	var
+	body: some View
+	{
+		HStack
+		{
+			VStack
+			{
+				Text("Debugging")
+					.fontWeight(.bold)
+					.padding(.bottom)
+				
+				Text("Checking Subscription")
+					.fontWeight(.bold)
+					.foregroundStyle(.gray)
+			}
+			.padding(4)
+			.border(.red)
+			.fixedSize(horizontal: false, vertical: false)
+			
+			Spacer(minLength: 0)
+		}
+		.padding()
+	}
 }

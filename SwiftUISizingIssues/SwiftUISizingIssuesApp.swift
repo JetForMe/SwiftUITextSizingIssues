7b022960f37fd6c50b201e7488effc55753fc748
Rick M <rmann@latencyzero.com>
2024-01-18 17:44:39 -0800

    Initial Commit

---
+++ SwiftUISizingIssues/SwiftUISizingIssuesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISizingIssuesApp.swift
+//  SwiftUISizingIssues
+//
+//  Created by Rick Mann on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISizingIssuesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
